e481de1b0ee88f7d6dee14feff9c8b15627036aa
Artem Levin <leoartemlev@yandex.ru>
2023-09-09 00:29:43 +0500

    fix templates practise

--- Templates/templates/pages/views.py
+++ Templates/templates/pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, '../templates/templates/home.html')
+    return render(request, 'home.html')
 
 def about(request):
-    return render(request, '../templates/templates/about.html')
+    return render(request, 'about.html')
